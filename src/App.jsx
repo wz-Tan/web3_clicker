499fd0215962682867ce802f1eb91a4893bd4966
wz-Tan <wenzhe1423@gmail.com>
2025-07-05 17:18:31 +0800

    Connected to Wallet. May refactor to change the tutorial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import { ConnectButton, useCurrentAccount, useSuiClientQuery  } from '@mysten/dapp-kit';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  var accountNumber;
 
   return (
+
     <section className='home w-screen h-screen bg-[#292d3e]'>
       <div className="max-w-4/5 mx-auto flex flex-col items-center">
         <h1 className='py-5 text-4xl text-transparent bg-[url(/gradient.jpeg)] bg-clip-text'>Web3 Clicker</h1>
@@ -17,12 +20,55 @@ function App() {
           }}
           ><img className='w-50 h-50 cursor-pointer' src="/smile.png" alt="" /></btn>
           <h1 className='text-white text-3xl'>Times Clicked : {count}</h1>
+
+          {/* Connects to Your Wallet */}
+          <ConnectButton/>
+
+          {/* Connected Account Returns Variable */}
+          <ConnectedAccount/>
         </div>
       </div>
 
     </section>
 
+
   )
+}
+
+function ConnectedAccount() {
+	const account = useCurrentAccount();
+
+	if (!account) {
+		return null;
+	}
+
+	return (
+  <>
+   <div className='bg-white p-3 rounded-xl'>Connected to {account.address}</div>
+   <OwnedObjects address={account.address} />
+   </>
+  )
+}
+
+function OwnedObjects({ address }) {
+	const { data } = useSuiClientQuery('getOwnedObjects', {
+		owner: address,
+	});
+	if (!data) {
+		return null;
+	}
+
+	return (
+		<ul>
+			{data.data.map((object) => (
+				<li key={object.data?.objectId}>
+					<a href={`https://example-explorer.com/object/${object.data?.objectId}`}>
+						{object.data?.objectId}
+					</a>
+				</li>
+			))}
+		</ul>
+	);
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import '@mysten/dapp-kit/dist/index.css';
 
+import { SuiClientProvider, WalletProvider } from '@mysten/dapp-kit';
+import { getFullnodeUrl } from '@mysten/sui/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+const networks = {
+  devnet: { url: getFullnodeUrl('devnet') },
+  mainnet: { url: getFullnodeUrl('mainnet') },
+};
+
+//Wrap the app with a client provider (queries hooks from the dApp Kit)
+//Then wrap with sui client to connect to network
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <SuiClientProvider networks={networks} defaultNetwork='devnet'>
+        <WalletProvider>
+          <App />
+        </WalletProvider>
+
+      </SuiClientProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
